ee25cc3b1d0db52478bae4f0272ca6ebbfaa12d4
Alvish Ramani <alvishramani.dev@gmail.com>
2025-01-13 14:07:20 +0530

    changed app name

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,6 +7,7 @@ class TrainingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Trainings App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
       ),
