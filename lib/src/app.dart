b1693d2e3b77d6ba0d720562b862d74341ba8f16
Alvish Ramani <alvishramani.dev@gmail.com>
2025-01-13 14:05:18 +0530

    setup project structure

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TrainingApp extends StatelessWidget {
+  const TrainingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Trainings App',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: Container(),
+    );
+  }
+}
